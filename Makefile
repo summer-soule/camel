f8309d073f9806ac803478db356861c7a7e46f1e
summer-soule <m.fleshka@gmail.com>
2024-12-20 07:49:23 +0300

    add & upd: solution to p2s2e2, upd Makefile

--- Makefile
+++ Makefile
@@ -8,13 +8,17 @@ PART2=$(SRCDIR)/part2
 
 # Exercises
 p2s1e1=$(BUILDDIR)/part2/section1/ex1
+p2s2e2=$(BUILDDIR)/part2/section2/ex2
 
-TARGETS=$(p2s1e1)
+TARGETS=$(p2s1e1) $(p2s2e2)
 
 .PHONY=all
 all: $(TARGETS)
 
 $(p2s1e1): $(SRCDIR)/part2/section1/ex1.c | $(BUILDDIR)
+	$(CC) -std=c99 -g $^ -o $@
+
+$(p2s2e2): $(SRCDIR)/part2/section2/ex2.c | $(BUILDDIR)
 	$(CC) -std=c99 -g $^ -o $@
 
 $(BUILDDIR):
---
+++ src/part2/section2/ex2.c
@@ -0,0 +1,43 @@
+//-----------------------------------------------------------------------------
+//
+// Task: Consider the program. Identify the directives and statements
+// 		 in this program
+//
+// Q1:	 What output does the program produce?
+//
+// Date: 2024-12-20
+// Time: 7:36 AM
+//
+//-----------------------------------------------------------------------------
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Parkinson's Law:\nWork expands so as to ");
+	printf("fill the time\n");
+	printf("available for its completion.\n");
+	return 0;
+}
+
+//-----------------------------------------------------------------------------
+// 
+// Directives:
+//
+// #include <stdio.h> - include content of stdio.h into this file
+//
+// Statements:
+//
+// printf("Parkinson's Law:\nWork expands so as to ");
+// printf("fill the time\n");
+// printf("available for its completion.\n");
+// return 0;
+//
+// Q1:	 What output does the program produce?
+// A1:
+//
+// Parkinson's Law:
+// Work expands so as to fill the time
+// available for its completion.
+//
+// ----------------------------------------------------------------------------
