889f1f87d19a0ea719033f4d545c7896d6baf156
summer-soule <m.fleshka@gmail.com>
2024-12-20 07:23:56 +0300

    add: solution to p2s1e1

---
+++ src/part2/section1/ex1.c
@@ -0,0 +1,18 @@
+//-----------------------------------------------------------------------------
+//
+// Task: create and run Kernighan and Ritchie's famous "hello, world" program
+//
+// Date: 2024-12-18
+// Time: 7:31 AM
+//
+//-----------------------------------------------------------------------------
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello, world\n");
+}
+
+// Q: Do you get a warning message from the compiler?
+// A: No. Same with -Wall gcc option
