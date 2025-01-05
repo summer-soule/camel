ded43f45288f2163bf320b379dcc7f38cedde735
summer-soule <m.fleshka@gmail.com>
2025-01-05 09:43:44 +0300

    add: solution to p2s2e5

---
+++ src/part2/section7/ex5.c
@@ -0,0 +1,17 @@
+//-----------------------------------------------------------------------------
+//
+// Task: Consider the program. Identify the directives and statements
+// 		 in this program
+//
+// Q1:	 Which of the following are not legal C identifiers?
+// (a)	100_bottles
+// (b)	_100_bottles
+// (c)	one__hundred__bottles
+// (d)	bottles_by_the_hundred_
+//
+// A1: (a) is illegal because starts with digit
+//
+// Date: 2025-1-5
+// Time: 9:42 AM
+//
+//-----------------------------------------------------------------------------
