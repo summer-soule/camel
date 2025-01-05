a315faff95a3f9b3446ae11a188cd151f943409c
summer-soule <m.fleshka@gmail.com>
2025-01-05 09:58:14 +0300

    add: solution to p2s7e7

---
+++ src/part2/section7/ex7.c
@@ -0,0 +1,21 @@
+//-----------------------------------------------------------------------------
+//
+// Q: Which of the following are keywords in C?
+// (a)	for
+// (b)	If
+// (c)	main
+// (d)	printf
+// (e)	while
+//
+// A: a, e
+//
+// Add.:
+// (b) "If" starts with capital i, but keyword "if" starts with lowercase "i"
+// (c) "main" is the name of main funcition that can be changed
+// (d) "printf" is the name of function from stdio.h and can be redefined
+//
+//
+// Date: 2025-1-5
+// Time: 9:57 AM
+//
+//-----------------------------------------------------------------------------
