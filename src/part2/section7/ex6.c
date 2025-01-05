284438a15ab76c9d72ba2c144d36a5afd3ed7b3f
summer-soule <m.fleshka@gmail.com>
2025-01-05 09:49:52 +0300

    add: solution to p2s2e6

---
+++ src/part2/section7/ex6.c
@@ -0,0 +1,13 @@
+//-----------------------------------------------------------------------------
+//
+// Q: Why is it not a good idea for an identifier to contain more one
+// adjacent underscore (as in current ___balance, for example)?
+//
+// A: Some compilers treat certain identifiers as additional keywords.
+// It can cause an error during compilation or linking.
+//
+//
+// Date: 2024-12-20
+// Time: 7:36 AM
+//
+//-----------------------------------------------------------------------------
