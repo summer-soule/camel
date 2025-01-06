261e51fa74db07a699612cc247981c557f0ac2be
summer-soule <m.fleshka@gmail.com>
2025-01-06 09:00:15 +0300

    add: solution to p2proj1

---
+++ src/part2/projects/project1.c
@@ -0,0 +1,26 @@
+//-----------------------------------------------------------------------------
+//
+// Task: Write a program that uses printf to display the following picture
+// on the screen:
+//
+//         *
+//        *
+//       *
+//      *
+// *   *
+//  * *
+//   *
+//
+// Date: 2025-1-6
+// Time: 7:02 AM
+//
+//-----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("       *\n      *\n     *\n*   *\n * *\n  *\n");
+	exit(EXIT_SUCCESS);
+}
