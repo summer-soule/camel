ce484a9845c247010537160e26631080511e4a97
summer-soule <m.fleshka@gmail.com>
2025-01-06 07:03:05 +0300

    add: solution to p2s8e10

---
+++ src/part2/section8/ex10.c
@@ -0,0 +1,28 @@
+//-----------------------------------------------------------------------------
+//
+// Q: In the dweight.c program (Section 2.4), which spaces are essential?
+// 
+//
+// #include <stdio.h>
+// 
+// int main(void)
+// {
+// 	printf("Parkinson's Law:\nWork expands so as to ");
+// 	printf("fill the time\n");
+// 	printf("available for its completion.\n");
+// 	return 0;
+// }
+//
+// A: 
+//
+// #include <stdio.h>
+// 		   ^
+// int main(void)
+//    ^
+// return 0;
+//       ^
+//
+// Date: 2025-1-6
+// Time: 7:02 AM
+//
+//-----------------------------------------------------------------------------
