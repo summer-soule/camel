41eae0fa4ff21974104a969ca38ccb831c1cd9ad
summer-soule <m.fleshka@gmail.com>
2025-01-10 11:46:15 +0300

    add: solution to p3s2e3

---
+++ src/part3/section2/ex3.c
@@ -0,0 +1,26 @@
+//-----------------------------------------------------------------------------
+//
+// Task: For each of the following pairs of scanf format strings, indicate
+// whether or not the two strings are equivalent. If they're not, show how they
+// can be distinguished.
+//
+// (a) "%d"			versus " %d"
+// 		equal: scanf skipping whitespaces even with "%d" format
+//
+// (b) "%d-%d-%d"	versus "%d -%d -%d"
+// 		unequal: "%d-%d-%d"
+// 					^--^------- first character after values MUST be the "-"
+// 				 "%d -%d -%d"
+// 				 	^---^------ can be 0 or unlimited white-space chars
+//
+// (c) "%f"			versus "%f "
+//		equal: after value can be 0 or unlimited white-space chars
+//
+// (d) "%f,%f"		versus "%f, %f"
+// 		equal: "%f, %f"
+// 				   ^----------- can be 0 or unlimited white-space char
+//
+// Date: 2025-1-10
+// Time: 10:50 AM
+//
+//-----------------------------------------------------------------------------
