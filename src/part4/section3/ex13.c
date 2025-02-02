d52f73a9cdbfa4f4763d6df070bd18705bf553f5
summer-soule <m.fleshka@gmail.com>
2025-02-02 04:51:02 +0300

    add: solution to p4s3e13

---
+++ src/part4/section3/ex13.c
@@ -0,0 +1,13 @@
+/*
+*
+* Task: Only one of the expression ++i is exactly the same as (i += 1);
+* which is it? Justify your answer.
+*
+* A: i = (i + 1);
+* This statement have a highest priority because i += 1 expands to parenthesed
+* form.
+*
+* Date: 2025-2-02
+* Time: 04:45 AM
+*
+*/
