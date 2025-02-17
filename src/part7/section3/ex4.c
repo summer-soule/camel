0fc5d6d5d085edb14fccdba591c9b29d19a51b7d
summer-soule <m.fleshka@gmail.com>
2025-02-17 13:21:38 +0300

    add: solution to p7s3e4

---
+++ src/part7/section3/ex4.c
@@ -0,0 +1,39 @@
+/* 
+ *
+ * Task: If c is a variable of type char, which one of the following statements
+ * is illegal?
+ *
+ * (a) i += c; i has type int
+ * (b) c = 2 * c - 1;
+ * (c) putchar(c);
+ * (d) printf(c); | warning: passing argument 1 of 'printf' makes pointer from
+ * 						     integer without a cast 
+ * 				  | causes segfault
+ *
+ * Date: 2025-02-17
+ * Time: 13:04 PM
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int i;
+	char c;
+
+	c = '#';
+	i = 1;
+
+	printf("c: %c\n", c);
+	printf("i: %d\n", i);
+	printf("i += c: %d\n", i += c);
+
+	c = 2 * c - 1;
+	printf("c = 2 * c - 1: %c\n", c);
+	putchar(c);
+	printf(c);
+
+	exit(EXIT_SUCCESS);
+}
