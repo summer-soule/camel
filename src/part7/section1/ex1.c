24e3209c2dfa9de007aba377d59e53ac4897733e
summer-soule <m.fleshka@gmail.com>
2025-02-17 12:27:48 +0300

    add: solution to p7s1e1

---
+++ src/part7/section1/ex1.c
@@ -0,0 +1,24 @@
+/* 
+ *
+ * Task: Give the decimal value of each of the following integer constants.
+ *
+ * (a) 077 = 63
+ * (b) 0x77 = 119
+ * (c) 0XABC = 2748
+ *
+ * Date: 2025-02-17
+ * Time: 12:19 PM
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("077 = %d\n", 077);
+	printf("0x77 = %d\n", 0x77);
+	printf("0XABC = %d\n", 0XABC);
+
+	exit(EXIT_SUCCESS);
+}
