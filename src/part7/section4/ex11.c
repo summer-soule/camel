5b7c0eeee8de08fed8a821d85fc5b16f7d9f4efc
summer-soule <m.fleshka@gmail.com>
2025-02-18 11:56:11 +0300

    add: solution to p7s4e11

---
+++ src/part7/section4/ex11.c
@@ -0,0 +1,31 @@
+/* 
+ *
+ * Task: Suppose that i is a variable of type int, f is a variable of type
+ * float, and d is a variable of type double. What is the type of the
+ * expression i * f / d?
+ *
+ * A: double
+ *
+ * Date: 2025-02-18
+ * Time: 11:52 AM
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int i;
+	float f;
+	double d;
+
+	i = 2;
+	f = 3.2f;
+	d = 5.0f;
+
+	printf("Expression has int type: %f\n", i * f / d);
+	printf("type: double\n");
+
+	exit(EXIT_SUCCESS);
+}
