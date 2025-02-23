ecd18a3306da7238168ee04c90bddd6c30fe39b1
summer-soule <m.fleshka@gmail.com>
2025-02-23 22:27:10 +0300

    add: solution to p7proj7

--- src/part7/projects/project7.c
+++ src/part7/projects/project7.c
@@ -17,13 +17,36 @@
 int main(void) 
 {
 	int num1, denom1, num2, denom2, result_num, result_denom;
+	char op;
 
-	printf("Enter two fractions separated by a plus sign: ");
-	scanf("%d/%d+%d/%d", &num1, &denom1, &num2, &denom2);
+	printf("Enter two fractions separated by an operation sign (+, -, *, /): ");
+	scanf("%d/%d %c %d/%d", &num1, &denom1, &op, &num2, &denom2);
 
-	result_num = num1 * denom2 + num2 * denom1;
-	result_denom = denom1 * denom2;
-	printf("The sum is %d/%d\n", result_num, result_denom);
+	switch(op) {
+		case '+':
+			result_num = num1 * denom2 + num2 * denom1;
+			result_denom = denom1 * denom2;
+			printf("The sum is %d/%d\n", result_num, result_denom);
+			break;
+		case '-':
+			result_num = num1 * denom2 - num2 * denom1;
+			result_denom = denom1 * denom2;
+			printf("The subtraction is %d/%d\n", result_num, result_denom);
+			break;
+		case '*':
+			result_num = num1 * num2;
+			result_denom = denom1 * denom2;
+			printf("The multiplication is %d/%d\n", result_num, result_denom);
+			break;
+		case '/':
+			result_num = num1 * denom2;
+			result_denom = denom1 * num2;
+			printf("The division is %d/%d\n", result_num, result_denom);
+		default:
+			printf("Unknown operation!\n");
+			break;
+	}
+
 
 	exit(EXIT_SUCCESS);
 }
