3358e297ea4442b2f64bfd53bb59016ff40153a9
summer-soule <m.fleshka@gmail.com>
2025-03-16 23:33:36 +0300

    upd: rewrite help message and addheader as functions

--- addheader.sh
+++ addheader.sh
@@ -6,42 +6,54 @@
 # TODO: 
 # - check if header already exists (do nothing if true)
 
-header="/* 
+header="/*
  *
- * Task: 
+ * Task:
  *
- * Date: $(date '+%Y-%m-%d')
- * Time: $(date '+%I:%M %p')
- *
- */"
-
-usage="$(basename "$0") usage:
-[[-p number] [-s number] [-e number]]
-[[-p number] [-P number]]
-
-Example output:
-
-/* 
- *
- * Task: 
- *
- * Date: $(date '+%Y-%m-%d')
- * Time: $(date '+%I:%M %p')
+ * Date: "$(date '+%Y-%m-%d')"
+ * Time: "$(date '+%I:%M %p')"
  *
  */
 
-Required:
-	-p 		part
-	-s		section
-	-e		exercise
-	-P		project
-Optional:
-	-h		show help message"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	exit(EXIT_SUCCESS);
+}"
+
+display_help() {
+	echo "$(basename "$0") usage:"
+	echo "[[-p number] [-s number] [-e number]]"
+	echo "[[-p number] [-P number]]"
+	echo
+	echo "Example output:"
+	echo
+	echo "$header"
+	echo
+	echo "Required:"
+	echo "  -p  part"
+	echo "  -s  section"
+	echo "  -e  exercise"
+	echo "  -P  project"
+	echo "Optional:"
+	echo "	-h	show help message"
+}
+
+addheader() {
+	if [ -f ${target} ]; then
+		echo "File $target exists! Adding header..."
+		echo $header | cat - "$target" | tee $target
+		echo "Header added! Exiting..."
+	fi
+}
+
+target="./src/"
 
 while getopts ":p:s:P:e:h" option; do
 	case "$option" in
 		h)
-			echo ${usage}
+			display_help
 			exit 0
 			;;
 		p) 
@@ -52,9 +64,11 @@ while getopts ":p:s:P:e:h" option; do
 			;;
 		e) 
 			target+="ex${OPTARG}.c"
+			addheader
 			;;
 		P) 
 			target+="projects/project${OPTARG}.c"
+			addheader
 			;;
 		:)
 			echo "Option -${OPTARG} requires an argument."
@@ -66,11 +80,3 @@ while getopts ":p:s:P:e:h" option; do
 			;;
 	esac
 done
-
-target="./src/$target"
-
-if [ -f ${target} ]; then
-	echo "File $target exists! Adding header..."
-	echo $header | cat - "$target" | tee $target
-	echo "Header added! Exiting..."
-fi
