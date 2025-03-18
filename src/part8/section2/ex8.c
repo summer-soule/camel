098c9d2ebcf9026aecceb87a5a6af3d073037b5e
summer-soule <m.fleshka@gmail.com>
2025-03-18 09:58:40 +0300

    add: solution to p8s2e8

---
+++ src/part8/section2/ex8.c
@@ -0,0 +1,21 @@
+/*
+ *
+ * Task: Write a declaration for a two-dimensional array named
+ * temperature_readings that stores one month of hourly temperature readings.
+ * (For simplicity, assume that a month has 30 days.) The rows of the array
+ * should represent days of the month; the columns should represent hours
+ * of the day.
+ *
+ * A: int temperature_readings[30][24] = {0};
+ *
+ * Date: 2025-03-17
+ * Time: 12:59 PM
+ *
+ */
+
+#include <stdlib.h>
+
+int main(void) {
+	int temperature_readings[30][24] = {0};
+	exit(EXIT_SUCCESS);
+}
