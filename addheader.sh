553766de5aa5269c519c7683d639565b4e812f2d
summer-soule <m.fleshka@gmail.com>
2025-03-18 11:37:23 +0300

    upd: rewrite functions and correct project's header

--- addheader.sh
+++ addheader.sh
@@ -6,7 +6,29 @@
 # TODO: 
 # - check if header already exists (do nothing if true)
 
-header="/*
+
+
+display_help() {
+	echo "$(basename "$0") usage:"
+	echo "[[-p number] [-s number] [-e number]]"
+	echo "[[-p number] [-P number]]"
+	echo
+	echo "Example exercise output:"
+	echo
+	echo "$header_ex"
+	echo
+	echo "Required:"
+	echo "  -p  part"
+	echo "  -s  section"
+	echo "  -e  exercise"
+	echo "  -P  project"
+	echo "Optional:"
+	echo "	-h	show help message"
+}
+
+addheader_ex() {
+	if [ -f ${target} ]; then
+		local header_ex="/*
  *
  * Task:
  *
@@ -21,29 +43,33 @@ header="/*
 int main(void) {
 	exit(EXIT_SUCCESS);
 }"
-
-display_help() {
-	echo "$(basename "$0") usage:"
-	echo "[[-p number] [-s number] [-e number]]"
-	echo "[[-p number] [-P number]]"
-	echo
-	echo "Example output:"
-	echo
-	echo "$header"
-	echo
-	echo "Required:"
-	echo "  -p  part"
-	echo "  -s  section"
-	echo "  -e  exercise"
-	echo "  -P  project"
-	echo "Optional:"
-	echo "	-h	show help message"
+		echo "File $target exists! Adding header..."
+		echo "$header_ex" | cat - "$target" | tee $target
+		echo "Header added! Exiting..."
+	fi
 }
 
-addheader() {
+addheader_proj() {
 	if [ -f ${target} ]; then
+		local header_proj="/*
+ *
+ * Project "$1".
+ *
+ * Task:
+ *
+ * Date: "$(date '+%Y-%m-%d')"
+ * Time: "$(date '+%I:%M %p')"
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	exit(EXIT_SUCCESS);
+}"
 		echo "File $target exists! Adding header..."
-		echo "$header" | cat - "$target" | tee $target
+		echo "$header_proj" | cat - "$target" | tee $target
 		echo "Header added! Exiting..."
 	fi
 }
@@ -64,11 +90,12 @@ while getopts ":p:s:P:e:h" option; do
 			;;
 		e) 
 			target+="ex${OPTARG}.c"
-			addheader
+			addheader_ex
 			;;
 		P) 
 			target+="projects/project${OPTARG}.c"
-			addheader
+			echo $proj_num
+			addheader_proj "${OPTARG}"
 			;;
 		:)
 			echo "Option -${OPTARG} requires an argument."
