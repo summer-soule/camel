a6516a463484ebf6855a512acf543e47ce403902
summer-soule <m.fleshka@gmail.com>
2025-03-21 12:11:58 +0300

    add: solution to p8proj5

---
+++ src/part8/projects/project5.c
@@ -0,0 +1,51 @@
+/*
+ *
+ * Project 5.
+ *
+ * Task: Modify the interest.c program of Section 8.1 so that it compounds
+ * interest monthly instead of annually. The form of the output shouldn't
+ * change: the balance should still be shown at annual intervals.
+ *
+ * Date: 2025-03-18
+ * Time: 03:38 PM
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_RATES		((int) (sizeof(value) / sizeof(value[0])))
+#define INITIAL_BALANCE	100.00
+
+int main(void) {
+	int i, low_rate, num_years, year, month;
+	double value[5];
+
+	printf("Enter interest rate: ");
+	scanf("%d", &low_rate);
+
+	printf("Enter number of years: ");
+	scanf("%d", &num_years);
+
+	printf("\nYears");
+
+	for (i = 0; i < NUM_RATES; i++) {
+		printf("%6d%%", low_rate + i);
+		value[i] = INITIAL_BALANCE;
+	}
+
+	printf("\n");
+
+	for (year = 1; year <= num_years; year++) {
+		printf("%3d    ", year);
+
+		for (i = 0; i < NUM_RATES; i++) {
+			for (month = 1; month <= 12; month++)
+				value[i] += ((double) (low_rate + i) / 12) / 100.00 * value[i];
+			printf("%7.2f", value[i]);
+		}
+		printf("\n");
+	}
+
+	exit(EXIT_SUCCESS);
+}
