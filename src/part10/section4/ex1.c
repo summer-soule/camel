d72fc6975a85767c400ce13f640882af24574716
summer-soule <m.fleshka@gmail.com>
2025-03-31 11:53:24 +0300

    add: solution p10s4e1

---
+++ src/part10/section4/ex1.c
@@ -0,0 +1,45 @@
+/*
+ *
+ * Task: The following program outline shows only function definitions and
+ * variable declarations
+ *
+ * 	int a;
+ *
+ * 	void f(int b)
+ * 	{
+ * 		int c;
+ * 	}
+ *
+ * 	void g(void)
+ * 	{
+ * 		int d;
+ * 		{
+ * 			int e;
+ * 		}
+ * 	}
+ * 	
+ * 	int main(void)
+ * 	{
+ * 		int f;
+ * 	}
+ *
+ * For each of the following scopes, list all variable and parameter names
+ * in that scope:
+ *
+ * (a) The f function
+ * 	   > variables: (a(c)
+ *	   > parameter: b
+ * (b) The g function
+ * 	   > variables: a(d)
+ *	   > parameter: none
+ * (c) The block in which e is declared
+ * 	   > variables: a(d(e))
+ *	   > parameter: none
+ * (d) The main function
+ * 	   > variables: a(f))
+ *	   > parameter: none
+ *
+ * Date: 2025-03-31
+ * Time: 11:27 AM
+ *
+ */
