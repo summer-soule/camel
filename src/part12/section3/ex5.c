50b4563e5535957b20a7f118dee8171048a8666c
summer-soule <m.fleshka@gmail.com>
2025-04-11 00:35:05 +0300

    fix: warning with illegal comparison

--- src/part12/section3/ex5.c
+++ src/part12/section3/ex5.c
@@ -27,7 +27,7 @@ int main(void) {
 	int a[ARR_L] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int *p = a;
 
-	printf("p == a[0]: %d\n", p == a[0]);
+	//printf("p == a[0]: %d\n", p == a[0]);
 	printf("p == &a[0]: %d\n", p == &a[0]);
 	printf("*p == a[0]: %d\n", *p == a[0]);
 	printf("p[0] == a[0]: %d\n", p[0] == a[0]);
