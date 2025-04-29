2db035cd5a6d339086863e86c8150df223dd2aff
summer-soule <m.fleshka@gmail.com>
2025-04-29 04:23:34 +0300

    add: solution to p14s4e12

---
+++ src/part14/section4/ex12.c
@@ -0,0 +1,21 @@
+/*
+ *
+ * Task: Suppose that the macro M has been defined as follows:
+ *
+ * 		 	#define M 10
+ *
+ * 		 Which of the following tests will fail?
+ *
+ * 		 (a) #if M
+ * 		 (b) #ifdef M
+ * 		 (c) #ifndef M
+ * 		 (d) #if defined(M)
+ * 		 (e) #if !defined(M)
+ *
+ * A:
+ * 		 (a) and (c) will fail as M is defined
+ *
+ * Date: 2025-04-29
+ * Time: 03:40 AM
+ *
+ */
