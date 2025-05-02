20ea7d358a16c84f7e5c95526d7f1a12825441cc
summer-soule <m.fleshka@gmail.com>
2025-05-02 05:32:05 +0300

    upd: add missed variants

--- src/part15/section2/ex2.c
+++ src/part15/section2/ex2.c
@@ -2,12 +2,14 @@
  *
  * Task: Which of the following should not be put in a header file? Why not?
  *
- * 		 (a) Function prototypes
- * 		 (b) Function definitions
+ * 		 (a) Function prototypes - legit
+ * 		 (b) Function definitions - strongly not recommended:
  * 		 	 Function definitins should not be put in a header file.
  * 		 	 If header file with function definition will be included
  * 		 	 two or more times anywhere, then linker will see two
  * 		 	 functions which leads to error on linking stage.
+ * 		 (c) Macro definitions - legit
+ * 		 (d) Type definitions - legit
  *
  * Date: 2025-05-02
  * Time: 05:00 AM
