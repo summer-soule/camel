bbe2533a7472bf5f6579e8b01178843f24ffcb30
summer-soule <m.fleshka@gmail.com>
2025-08-27 12:56:24 +0300

    add: solution to p18s3e7

---
+++ src/part18/section3/ex7.c
@@ -0,0 +1,23 @@
+/*
+ *
+ * Task:
+ *		Suppose that we declare x to be a const object.
+ *		Which one of the following statements about x is false?
+ *
+ *		(a) If x is of type int, it can be used as the value of
+ *			a case label in a switch statement.
+ *			false
+ *
+ *		(b) The compiler will check that no assignment is made to x.
+ *			true
+ *
+ *		(c) x is subject to the same scope rules as variables.
+ *			true
+ *
+ *		(d) x can be of any type.
+ *			true
+ *
+ * Date: 2025-08-27
+ * Time: 12:37 PM
+ *
+ */
