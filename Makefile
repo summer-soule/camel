978276b3e4302c846b4703e3ff3553767455667b
summer-soule <m.fleshka@gmail.com>
2025-08-28 14:25:48 +0300

    add: solution to p18s4e8

--- Makefile
+++ Makefile
@@ -46,6 +46,7 @@ $(BINDIR)/part15/section4/ex6
 $(BINDIR)/part18/section2/ex2
 $(BINDIR)/part18/section2/ex3
 $(BINDIR)/part18/section2/ex5
+$(BINDIR)/part18/section4/ex8
 endef
 
 EXERCISE_TARGETS_C99 := $(filter-out $(EXERCISE_TARGETS_C89) $(EXERCISE_TARGETS_C99_EXCLUDE), $(EXERCISE_TARGETS_ALL))
---
+++ src/part18/section4/ex8.c
@@ -0,0 +1,28 @@
+/*
+ *
+ * Task:
+ *		Write a complete description of the type of x as
+ *		specified by each of the following declarations.
+ *
+ *		(a) char (*x[10])(int);
+ *			x - array of pointers to functions. Each function takes int
+ *			argument and returns a char.
+ *
+ *		(b) int (*x(int))[5];
+ *			x - is a function with int argument that returns array of 5
+ *			integers.
+ *
+ *		(c) float *(*x(void))(int);
+ *			x - is a function with no arguments that returns pointer
+ *			to a function with int argument that returns float.
+ *
+ *		(d) void (*x(int, void (*y)(int)))(int);
+ *			x - is a function with two arguments, where first takes
+ *			integer and second is a function y with int argument that
+ *			returns nothing; x returns pointer to a function with int
+ *			argument and no return.
+ *
+ * Date: 2025-08-27
+ * Time: 02:10 PM
+ *
+ */
