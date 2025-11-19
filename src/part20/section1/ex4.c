42fa6ce2636574c69a1532800b09f820a76b804d
summer-soule <m.fleshka@gmail.com>
2025-11-19 21:22:34 +0300

    add: solution to p20s1e4

---
+++ src/part20/section1/ex4.c
@@ -0,0 +1,28 @@
+/*
+ *
+ * Task:
+ *		In computer graphics, colors are often stored as three numbers,
+ *		representing red, green, and blue intensities. Suppose that
+ *		each number requires eight bits, and we'd like to store all
+ *		three values in a single long integer. Write a macro named
+ *		MK_COLOR with three parameters (the red, green, and blue
+ *		intensities). MK_COLOR should return a long in which the last
+ *		three bytes contain the red, green, and blue intensities,
+ *		with the red value as the last byte and the green value
+ *		as the next-to-last byte.
+ *
+ * Date: 2025-11-19
+ * Time: 08:21 PM
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MK_COLOR(r,g,b) ((long) (b) << 16 | (g) << 8 | (r))
+
+int main(void) {
+    printf("%lu\n", MK_COLOR(255, 255, 255));
+
+	exit(EXIT_SUCCESS);
+}
