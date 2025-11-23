94cecdc973b8e3dbd52e6311febaae23cb93e423
summer-soule <m.fleshka@gmail.com>
2025-11-24 02:25:14 +0300

    add: solution to p21s1e3

---
+++ src/part21/section1/ex3.c
@@ -0,0 +1,15 @@
+/*
+ *
+ * Task:
+ *		When a macro hides a function, which must come first in the header
+ *		file: the macro definition or the function prototype? Justify
+ *		your answer.
+ *
+ * Answer:
+ *		Function prototype must come first to show its existence to
+ *		preprocessor.
+ *
+ * Date: 2025-11-24
+ * Time: 02:11 AM
+ *
+ */
