bf0d784aa181cddf639ef7634b5c0fcd723dbffa
summer-soule <m.fleshka@gmail.com>
2025-12-14 00:10:30 +0300

    fix: confused by "%1s" from the book
    "%1s" was percieved as "%ls" because of font

--- src/part22/section3/ex8.c
+++ src/part22/section3/ex8.c
@@ -3,17 +3,12 @@
  * Task:
  * 		In previous chapters, we've used the scanf format string " %c"
  * 		when we wanted to skip white-space characters and read a nonblank
- * 		character. Some programmers use "%ls" instead. Are the two techniques
+ * 		character. Some programmers use "%1s" instead. Are the two techniques
  * 		equivalent? If not, what are the differences?
  *
  * Answer:
- * 		1. " %c" specifier skips white-space characters and reads only
- * 		   first character into char variable.
- * 		2. "%ls" specifier intended to work with strings of wchar_t (wide)
- * 		   characters.
- *
- *		As result, there is two big differences: char vs wchar_t types
- *		and single vs string expected respectively.
+ *		1. " %c" stores first nonblank character.
+ *		2. "%1s" stores first nonblank character and adds null character after.
  *
  * Date: 2025-12-10
  * Time: 10:23 AM
@@ -26,16 +21,16 @@
 
 int main(void) {
 	char *teststring = "    test";
-	char string1[120];
-	wchar_t string2[120];
+	char c;
+	char string2[5];
 
 	printf("test string: '%s'\n", teststring);
 
-	sscanf(teststring, " %c", string1);
-	printf("%s\n", string1);
+	sscanf(teststring, " %c", &c);
+	printf("%c\n", c);
 
-	sscanf(teststring, "%ls", string2);
-	printf("%ls\n", string2);
+	sscanf(teststring, "%1s", string2);
+	printf("%s\n", string2);
 
 	exit(EXIT_SUCCESS);
 }
