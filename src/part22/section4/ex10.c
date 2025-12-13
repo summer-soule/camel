b79bb16bb8daad040ec28f863515e78d428965cc
summer-soule <m.fleshka@gmail.com>
2025-12-14 01:01:19 +0300

    add: solution to p22s4e10

---
+++ src/part22/section4/ex10.c
@@ -0,0 +1,52 @@
+/*
+ *
+ * Task:
+ *		The fcopy.c program has one minor flaw: it doesn't check for errors
+ *		as it's writing to the destination file. Errors during writing are
+ *		rare, but do occasionally occur (the disc might become full,
+ *		for example). Show how to add the missing error check to the
+ *		program, assuming that we want it to display a message and terminate
+ *		immediately if an error occurs.
+ *
+ * Date: 2025-12-14
+ * Time: 12:14 AM
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	FILE *source_fp, *dest_fp;
+    int ch;
+
+    if (argc != 3) {
+		fprintf(stderr, "usage: fcopy source dest\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((source_fp = fopen(argv[1], "rb")) == NULL) {
+		fprintf(stderr, "Can't open %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((dest_fp = fopen(argv[2], "wb")) == NULL) {
+		fprintf(stderr, "Can't open %s\n", argv[2]);
+		fclose(source_fp);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((ch = getc(source_fp)) != EOF)
+        if (putc(ch, dest_fp) == EOF) {
+			fprintf(stderr, "getc: error to write\n");
+			fclose(source_fp);
+			fclose(dest_fp);
+			exit(EXIT_FAILURE);
+		}
+
+
+	fclose(source_fp);
+	fclose(dest_fp);
+
+	exit(EXIT_SUCCESS);
+}
