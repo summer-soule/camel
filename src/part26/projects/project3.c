47e74cdd0c83fd66fa08b156854eaa14962ee807
summer-soule <m.fleshka@gmail.com>
2026-04-28 10:32:00 +0300

    upd: rewrite notation

--- src/part26/projects/project3.c
+++ src/part26/projects/project3.c
@@ -27,9 +27,9 @@ void arrinit(int *, int);
 int compar(const void *, const void *);
 
 int main(void) {
-	int arr1[(size_t)10e2];
-	int arr2[(size_t)10e3];
-	int arr3[(size_t)10e4];
+	int arr1[(size_t)1e3];
+	int arr2[(size_t)1e4];
+	int arr3[(size_t)1e5];
 
 	arrinit(arr1, ARRSIZE(1));
 	arrinit(arr2, ARRSIZE(2));
